5f22bf8ba74689cb93fd47d815978eefc4c08233
HysenB <hysi2000@live.com>
2022-10-12 13:06:57 +0200

    adding feedback list, item, card component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
-import React from 'react';
+import { useState } from 'react';
+import Header from './components/Header'
+import FeedBackData from './data/FeedbackData';
+import FeedbackList from './components/FeedbackList';
 
 function App(){
-    const title = 'Blog Post';
-    const body = 'This is my blog post';
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
 
-    const loading = false;
-    const showComments = true;
+    const [feedback, setFeedback] = useState(FeedBackData);
 
-    const commentBlock = (
-        <div className='comments'>
-            <h3>Comments ({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-         </div>
-    );
 
-    if(loading) return <h1>Loading...</h1>
 
     return (
-        <div className='container'>
-            <h1>{title}</h1>
-            <p>{body}</p>
-            {showComments && commentBlock}
-            
-        </div>
-
+        <>
+            <Header />
+            <div className='container'>
+                <FeedbackList feedback={feedback} />
+            </div>
+        </>
     )
 }
+
+// function App(){
+//     const title = 'Blog Post';
+//     const body = 'This is my blog post';
+//     const comments = [
+//         {id: 1, text: 'Comment one'},
+//         {id: 2, text: 'Comment two'},
+//         {id: 3, text: 'Comment three'},
+//     ]
+
+//     const loading = false;
+//     const showComments = true;
+
+//     const commentBlock = (
+//         <div className='comments'>
+//             <h3>Comments ({comments.length})</h3>
+//             <ul>
+//                 {comments.map((comment, index) => (
+//                     <li key={index}>{comment.text}</li>
+//                 ))}
+//             </ul>
+//          </div>
+//     );
+
+//     if(loading) return <h1>Loading...</h1>
+
+//     return (
+//         <div className='container'>
+//             <h1>{title}</h1>
+//             <p>{body}</p>
+//             {showComments && commentBlock}
+            
+//         </div>
+
+//     )
+// }
 
 export default App
